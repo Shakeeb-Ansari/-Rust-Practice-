fac4cba64c86ff034091d54278f308ea3b8580a2
Shakeeb-Ansari <shakeeb100ansari@gmail.com>
2021-09-26 19:57:26 +0500

    Practice

---
+++ src/arrays.rs
@@ -0,0 +1,13 @@
+pub fn run() {
+    let mut arr: [i8;6]=[10,11,12,13,14,15];
+    println!("{:?}",arr);
+
+    println!("Value at 3rd index is {}", arr[3]);  // Printing a specific i.e 3rd index only
+
+    arr[1] = 25;        //Replacing or modifying specific index's value
+    println!("The changed value at 1st index is {:?}", arr);
+
+    let slice: &[i8] = &arr[1..5];
+    println!("Slice: {:?}", slice);
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![allow(non_snake_case)]
 //mod vars;
 //mod dtypes;
-mod tuples;
+//mod tuples;
+mod arrays;
 fn main() {
     //vars::run();
     //dtypes::run();
-    tuples::run();
+    //tuples::run();
+    arrays::run();
+
 }
