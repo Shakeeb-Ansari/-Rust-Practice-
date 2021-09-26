9733ae278fc70a25210360804c385dd89f779680
Shakeeb-Ansari <shakeeb100ansari@gmail.com>
2021-09-26 18:37:43 +0500

    Practice

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![allow(non_snake_case)]
 //mod vars;
-mod dtypes;
+//mod dtypes;
+mod tuples;
 fn main() {
     //vars::run();
-    dtypes::run();
+    //dtypes::run();
+    tuples::run();
 }
---
+++ src/tuples.rs
@@ -0,0 +1,4 @@
+pub fn run() {
+    let tup: (&str,f32,i64) = ("Edward",77.7,28);
+    println!("{} weighs {}Kg and is {} years old.", tup.0, tup.1, tup.2);
+}
